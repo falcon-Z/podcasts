752e9981e9ab65ab51439580a68770a4a5aa7837
Sai Madhan <saimadhan@outlook.in>
2022-10-14 11:27:51 +0000

    added full height

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import type { Component } from "solid-js";
 
 const App: Component = () => {
   return (
-    <div class="bg-black h-full">
+    <div class="bg-black text-white h-full">
       <p>Home</p>
     </div>
   );
